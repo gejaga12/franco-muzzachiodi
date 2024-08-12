4327a6cecc3a2a520c19a99214fc2400c6eb0ce9
gejaga12 <gejaga12@hotmail.com>
2024-08-12 13:37:11 -0300

    Fix: arreglo responsividad de location

--- src/app/components/info/LocationHighlight.tsx
+++ src/app/components/info/LocationHighlight.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import Image from 'next/image';
-import { FaPhoneAlt, FaMapMarkerAlt, FaWhatsapp, FaVideo } from 'react-icons/fa'; // Importa FaVideo para el ícono de videollamada
-import { FiMessageSquare } from 'react-icons/fi'; // Importa FiMessageSquare para el ícono de consulta
+import { FaMapMarkerAlt, FaWhatsapp, FaVideo } from 'react-icons/fa';
+import { FiMessageSquare } from 'react-icons/fi';
 
 interface LocationHighlightProps {
   buenosAiresImageSrc: string;
@@ -10,65 +10,65 @@ interface LocationHighlightProps {
 
 const LocationHighlight: FC<LocationHighlightProps> = ({ buenosAiresImageSrc, corrientesImageSrc }) => {
   return (
-    <section className="flex flex-col md:flex-row h-screen" id='location'>
+    <section className="flex flex-col md:flex-row h-auto md:h-screen" id='location'>
       
       {/* Ciudad Autónoma de Buenos Aires */}
-      <div className="relative bg-black w-full md:w-1/2 h-1/2 md:h-full">
+      <div className="relative bg-black w-full md:w-1/2 h-96 md:h-full">
         <Image 
           src={buenosAiresImageSrc} 
           alt="Ciudad Autónoma de Buenos Aires" 
           layout="fill" 
           objectFit="cover" 
-          className="opacity-35"
+          className="opacity-50"
         />
-        <div className="absolute inset-0 flex items-center justify-center flex-col">
-          <h2 className="text-white text-2xl md:text-4xl font-bold text-center">
+        <div className="absolute inset-0 flex items-center justify-center flex-col p-6 md:p-10">
+          <h2 className="text-white text-xl md:text-3xl font-bold text-center">
             ATENDEMOS EN <br /> CIUDAD DE BUENOS AIRES
           </h2>
-          <div className="flex mt-4 space-x-4  text-white items-center">            
-            <FaMapMarkerAlt size={20} />
-            <h3 className='text-xl'>Av. Corrientes 2458</h3>            
+          <div className="flex mt-2 md:mt-4 space-x-2 md:space-x-4 text-white items-center">            
+            <FaMapMarkerAlt size={16} md={20} />
+            <h3 className='text-sm md:text-lg'>Av. Corrientes 2458</h3>            
           </div>
-          <div className="mt-6 flex space-x-4">
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FiMessageSquare size={20} className="mr-2" /> CONSULTA
+          <div className="mt-4 md:mt-6 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 w-full">
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FiMessageSquare size={16} className="mr-2" /> CONSULTA
             </button>
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FaVideo size={20} className="mr-2" /> AGENDAR VIDEO LLAMADA
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FaVideo size={16} className="mr-2" /> AGENDAR VIDEO LLAMADA
             </button>
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FaWhatsapp size={20} className="mr-2" /> WHATSAPP
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FaWhatsapp size={16} className="mr-2" /> WHATSAPP
             </button>
           </div>
         </div>
       </div>
 
       {/* Ciudad de Corrientes Capital */}
-      <div className="relative bg-black w-full md:w-1/2 h-1/2 md:h-full">
+      <div className="relative bg-black w-full md:w-1/2 h-96 md:h-full">
         <Image 
           src={corrientesImageSrc} 
           alt="Ciudad de Corrientes Capital" 
           layout="fill" 
           objectFit="cover" 
-          className="opacity-35"
+          className="opacity-50"
         />
-        <div className="absolute inset-0 flex items-center justify-center flex-col">
-          <h2 className="text-white text-2xl md:text-4xl font-bold text-center">
+        <div className="absolute inset-0 flex items-center justify-center flex-col p-6 md:p-10">
+          <h2 className="text-white text-xl md:text-3xl font-bold text-center">
             ATENDEMOS EN <br /> CIUDAD DE CORRIENTES CAPITAL
           </h2>
-          <div className="flex mt-4 space-x-4 text-white items-center">            
-            <FaMapMarkerAlt size={20} />
-            <h3 className='text-xl'>Av. 3 de Abril 2458</h3>            
+          <div className="flex mt-2 md:mt-4 space-x-2 md:space-x-4 text-white items-center">            
+            <FaMapMarkerAlt size={16} />
+            <h3 className='text-sm md:text-lg'>Av. 3 de Abril 2458</h3>            
           </div>
-          <div className="mt-6 flex space-x-4">
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FiMessageSquare size={20} className="mr-2" /> CONSULTA
+          <div className="mt-4 md:mt-6 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 w-full">
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FiMessageSquare size={16} className="mr-2" /> CONSULTA
             </button>
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FaVideo size={20} className="mr-2" /> AGENDAR VIDEO LLAMADA
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FaVideo size={16} className="mr-2" /> AGENDAR VIDEO LLAMADA
             </button>
-            <button className="flex items-center border-2 bg-black bg-opacity-50 border-white text-white px-4 py-2 rounded-lg">
-              <FaWhatsapp size={20} className="mr-2" /> WHATSAPP
+            <button className="flex items-center justify-center border-2 bg-black bg-opacity-50 border-white text-white px-3 py-2 text-sm rounded-lg w-full md:w-auto">
+              <FaWhatsapp size={16} className="mr-2" /> WHATSAPP
             </button>
           </div>
         </div>
